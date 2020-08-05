74e3881a969eb137abe44901e39ac4dc8eed1fb6
LeopoldFrilot <lfrilo1@lsu.edu>
2020-08-05 15:52:25 -0500

    Initialized Unity Project

---
+++ New Unity Project/Assets/SceneSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
